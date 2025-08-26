10821164d0d10b26c0afdb3264c240304a70ee1a
WooyoungKwon <108107570+WooyoungKwon@users.noreply.github.com>
2025-08-26 17:17:05 +0900

    [Gold V] Title: LCS, Time: 440 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -6,14 +6,14 @@ b = sys.stdin.readline().rstrip()
 a_len = len(a)
 b_len = len(b)
 
-dp = [[0] * (a_len + 1) for _ in range(b_len + 1)]
+dp = [[0] * (b_len + 1) for _ in range(a_len + 1)]
 
-for i in range(1, b_len + 1):
-    for j in range(1, a_len + 1):
+for i in range(1, a_len + 1):
+    for j in range(1, b_len + 1):
 
-        if a[j - 1] == b[i - 1]:
+        if a[i - 1] == b[j - 1]:
             dp[i][j] = dp[i-1][j-1] + 1
         else:
             dp[i][j] = max(dp[i][j-1], dp[i-1][j])
 
-print(dp[b_len][a_len])
+print(dp[a_len][b_len])
