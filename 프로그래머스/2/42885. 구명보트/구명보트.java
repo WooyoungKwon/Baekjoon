a3b9e4072e726e6bca28079088d45a75b9a5fadc
WooyoungKwon <108107570+WooyoungKwon@users.noreply.github.com>
2025-11-10 13:34:41 +0900

    [level 2] Title: 구명보트, Time: 11.49 ms, Memory: 57.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        
+        while(left <= right) {
+            if (left == right) {
+                answer += 1;
+                break;
+            }
+            if (people[left] + people[right] <= limit) {
+                left++;
+                right--;
+            } else {
+                right--;
+            }
+            answer += 1;
+        }
+        
+        return answer;
+    }
+}
